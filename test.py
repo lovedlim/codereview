886efc5fbca01f464974b61409ac82818c099d25
Kim Tae Heon <danmujicafe@gmail.com>
2024-03-11 17:06:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-def add_numbers(a, b):
+def select_and_square_even_numbers(numbers):
     """
-    두 숫자를 더하는 함수
+    주어진 리스트에서 짝수를 선택하여 제곱한 값들을 새로운 리스트에 저장하는 함수
     """
-    return a + b
+    squared_even_numbers = []  # 짝수를 제곱한 값들을 저장할 리스트
 
-# 사용자로부터 두 숫자를 입력 받음
-num1 = float(input("첫 번째 숫자를 입력하세요: "))
-num2 = float(input("두 번째 숫자를 입력하세요: "))
+    for num in numbers:
+        if num % 2 == 0:  # 짝수인 경우에만 제곱하여 리스트에 추가
+            squared_even_numbers.append(num ** 2)
 
-# 두 숫자를 더하고 결과 출력
-result = add_numbers(num1, num2)
-print("두 숫자의 합은:", result)
+    return squared_even_numbers
+
+# 테스트를 위한 입력 리스트
+input_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 주어진 리스트에서 짝수를 선택하여 제곱한 값들을 가져옴
+result = select_and_square_even_numbers(input_numbers)
+
+# 결과 출력
+print("주어진 리스트에서 짝수를 선택하여 제곱한 값들:", result)
